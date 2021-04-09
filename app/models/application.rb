1fd173c563748cc5f924882848766aec5eb78bc0
joemecha <jwmecha@gmail.com>
2021-04-09 17:54:22 -0500

    Add dependent destroy to model associations

--- app/models/application.rb
+++ app/models/application.rb
@@ -5,6 +5,6 @@ class Application < ApplicationRecord
   validates :state, presence: true
   validates :zip_code, presence: true, numericality: true
 
-  has_many :application_pets
+  has_many :application_pets, dependent: :destroy 
   has_many :pets, through: :application_pets
 end
